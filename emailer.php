a6fa98286d04ba185346c960a52a2823086b6d59
Skelpy Coin <skelpycoin@gmail.com>
2019-01-03 17:58:09 +0100

    Update emailer.php

--- emailer.php
+++ emailer.php
@@ -15,29 +15,39 @@
 		printf("Connect failed: %s\n", $mysqli->connect_error);
 		exit();
 	}
-	// Escape user inputs for security
+	 // Escape user inputs for security
 	$email = mysqli_real_escape_string($mysqli, $_REQUEST['email']);
-	
+	if ($email != ""){
 	if (filter_var($email, FILTER_VALIDATE_EMAIL)) {	
-	$sql = "SELECT * FROM {$table} WHERE email = '{$email}'";
-	$result = mysqli_query($mysqli,$sql);
-	$num_rows = intval(mysqli_num_rows($result));
 
-	if($num_rows > 0){
-		echo "You are alredy registered!";
-	}else{
-		$sql = "INSERT INTO {$table} (email) values('{$email}') ";
+		$sql = "SELECT * FROM {$table} WHERE email = '{$email}'";
 		$result = mysqli_query($mysqli,$sql);
-		if (!$result) {
-			die('Invalid query: ' . mysqli_error());
+		$num_rows = intval(mysqli_num_rows($result));
+
+		if($num_rows > 0){
+
+			echo "You are alredy registered!";
+
 		}else{
-			echo "Congratulations now you are part of our big family. Welcome!";
+
+			$sql = "INSERT INTO {$table} (email) values('{$email}') ";
+			$result = mysqli_query($mysqli,$sql);
+			if (!$result) {
+				die('Invalid query: ' . mysqli_error());
+			}else{
+				echo "Congratulations now you are part of our big family. Welcome!";
+			}
+
 		}
-	}
 	} else {
-	
-		 echo "Email address '$email' is considered invalid.\n"
-		
+
+		 echo "Email address '$email' is considered invalid.\n";
+
+	}
+	}else{
+
+		echo "The field is void... Are you kidding me?";
+
 	}
 
 ?>
